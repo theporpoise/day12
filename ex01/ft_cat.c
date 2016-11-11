7a7956b6fa3acd8429c6daa27b166151519eaff8
Matthew Gould <matt@everettgould.com>
2016-11-10 19:41:29 -0800

    updating cat error messages

--- ex01/ft_cat.c
+++ ex01/ft_cat.c
@@ -6,12 +6,35 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 17:08:48 by mgould            #+#    #+#             */
-/*   Updated: 2016/11/10 18:41:19 by mgould           ###   ########.fr       */
+/*   Updated: 2016/11/10 19:27:52 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
+#include <errno.h>
 #define SIZE 10
+
+void	ft_error(int error, char *file_name)
+{
+	if (error == 2)
+	{
+		ft_putstr("cat: " );
+		ft_putstr(file_name);
+		ft_putstr(": No such file or directory\n");
+	}
+	if (error == 21)
+	{
+		ft_putstr("cat: " );
+		ft_putstr(file_name);
+		ft_putstr(": Is a directory\n");
+	}
+	if (error == 13)
+	{
+		ft_putstr("cat: " );
+		ft_putstr(file_name);
+		ft_putstr(": Permission denied\n");
+	}
+}
 
 void	ft_oprint(char *file)
 {
@@ -22,14 +45,18 @@ void	ft_oprint(char *file)
 	fd = open(file, O_RDONLY);
 	if (fd < 0)
 	{
-		ft_putstr("cat: ");
-		ft_putstr(file);
-		ft_putstr(": No such file or directory\n");
+		ft_error(errno, file);
+		return ;
 	}
 	while ((ret = read(fd, buf, SIZE)) > 0)
 	{
 		buf[ret] = '\0';
 		ft_putstr(buf);
+	}
+	if (ret < 0)
+	{
+		ft_error(errno, file);
+		return ;
 	}
 	close(fd);
 }
